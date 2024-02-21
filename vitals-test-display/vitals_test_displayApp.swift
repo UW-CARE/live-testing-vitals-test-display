8cb623c0ef50045b110c70959d7ff7ebae5bec75
Lucas Wang <lucas.wang@tuta.io>
2024-02-21 15:13:02 -0800

    Initial Commit

---
+++ vitals-test-display/vitals_test_displayApp.swift
@@ -0,0 +1,17 @@
+//
+//  vitals_test_displayApp.swift
+//  vitals-test-display
+//
+//  Created by Lucas Wang on 2/21/24.
+//
+
+import SwiftUI
+
+@main
+struct vitals_test_displayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
